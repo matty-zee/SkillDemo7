db3b09413f028b7a6d2d93a988eddb2b71f59591
matty-zee <97648068+matty-zee@users.noreply.github.com>
2022-02-03 14:54:11 -0800

    Making a failing case for testTriple

--- SkillDemo7.java
+++ SkillDemo7.java
@@ -1,1 +1,5 @@
-public class SkillDemo7{}
+public class SkillDemo7{
+    public static int triple(int x){
+        return 3*x;
+    }
+}
---
+++ SkillDemo7Test.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemo7Test {
+    @Test
+    public void testTriple(){
+        assertEquals(2, SkillDemo7.triple(2));
+    }
+}
